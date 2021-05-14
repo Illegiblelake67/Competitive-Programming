66ac2bd76ef940a5fafdc2c4a93201023372031f
Illegiblrlake67 <nkondam01@gmail.com>
2021-05-13 21:29:30 -0400

    solved Camel case problem

---
+++ Codewars/Java/Break_camelCasing.java
@@ -0,0 +1,40 @@
+/*
+Complete the solution so that the function will break up camel casing,
+#using a space between words.
+
+#Example
+
+#{}"camelCasing"  =>  "camel Casing"
+#{}"identifier"   =>  "identifier"
+#{}""             =>  ""
+*/
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+
+class Solution {
+  public static String camelCase(String input) {
+    String nonCamelCasing ="";
+    for (char ch : input.toCharArray()){
+      if (ch == Character.toUpperCase(ch)) {
+        nonCamelCasing += " ";
+        nonCamelCasing += ch;
+      }else {
+        nonCamelCasing += ch;
+      }
+    }
+    return nonCamelCasing;
+
+  }
+}
+
+public class SolutionTest {
+    @Test
+    public void tests() {
+      assertEquals( "Incorrect", "camel Casing", Solution.camelCase("camelCasing"));
+      assertEquals( "Incorrect", "camel Casing Test", Solution.camelCase("camelCasingTest"));
+      assertEquals( "Incorrect", "camelcasingtest", Solution.camelCase("camelcasingtest"));
+    }
+}
---
+++ Codewars/Ruby/Break_camelCase.rb
@@ -0,0 +1,24 @@
+#Complete the solution so that the function will break up camel casing,
+#using a space between words.
+
+#Example
+
+#{}"camelCasing"  =>  "camel Casing"
+#{}"identifier"   =>  "identifier"
+#{}""             =>  ""
+
+def solution(string)
+  result = ""
+  string.each_char do |char|
+    if char == char.upcase
+      result += " "
+      result += char
+    else
+      result += char
+    end
+  end
+  result
+end
+
+puts solution('camelCasing') #'camel Casing')
+puts solution('camelCasingTest') #'camel Casing Test'
