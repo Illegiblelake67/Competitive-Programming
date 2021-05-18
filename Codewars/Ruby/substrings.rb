e12913856b0d518cb7fbf5398445061ed7ef6a36
Illegiblrlake67 <nkondam01@gmail.com>
2021-05-18 00:34:22 -0400

    clean up

--- Codewars/Ruby/substrings.rb
+++ Codewars/Ruby/substrings.rb
@@ -1,6 +1,6 @@
-def substrings(words, dictionary)
-  count = Hash.new{0}
-  dictionary.each { |ele| count[ele] += 1 if words.include?(ele)}
+def substrings(string, dictionary)
+  count = {}
+  dictionary.each {|word| count[word] = string.downcase.scan(/(?=#{word})/).count if string.downcase.include?(word)}
   return count
 end
 
@@ -8,5 +8,6 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 #["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 print substrings("below", dictionary)
 #{ "below" => 1, "low" => 1 }
+puts
 print substrings("Howdy partner, sit down! How's it going?", dictionary)
 #{ "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
